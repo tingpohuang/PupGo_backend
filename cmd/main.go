ce9e5447a440f1a33102cb7cfeaaa335197a4112
Ting-Po <tim840815@gmail.com>
2021-11-07 15:20:10 -0800

    used viper to set up config

--- cmd/main.go
+++ cmd/main.go
@@ -3,20 +3,27 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 	"net/http"
 )
 
+func init() {
+	viper.SetConfigName("config")     // name of config file (without extension)
+	viper.SetConfigType("json")       // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath("./configs/") // path to look for the config file in
+	err := viper.ReadInConfig()       // Find and read the config file
+	if err != nil {                   // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %w \n", err))
+	}
+}
+
 func main() {
 	r := gin.Default()
-	fmt.Println(r)
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
 	})
 
-	r.Run()
-
-
-
+	r.Run(viper.GetString("server.address"))
 
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/tingpo/pupgobackend
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.7.4
+require (
+	github.com/gin-gonic/gin v1.7.4
+	github.com/spf13/viper v1.9.0 // indirect
+)
